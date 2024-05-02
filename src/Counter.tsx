9a894b412a189934b453caa093d2c48d62cf22d9
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2024-05-02 23:58:01 +0300

    new

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,46 +1,9 @@
-import { useState } from "react";
-import { Button } from "./Button";
-import { CounterBody } from "./CounterBody";
-import { SettingsMenu } from "./SettingsMenu";
-import s from "./Counter.module.css";
-
 export function Counter() {
-  const [counter, setCounter] = useState<number>(0);
-  const [openSettings, setOpenSettings] = useState<boolean>(false);
-  const isCounterToHight = counter >= 5;
-
-  const increaseCounter = () => {
-    if (!isCounterToHight) {
-      setCounter(counter + 1);
-    }
-  };
-
-  const onClickOpenSettingsHeandler = () => {
-    setOpenSettings(!openSettings);
-  };
-
   return (
-    <div className={s.container}>
-      {openSettings ? (
-        <CounterBody isCounterToHight={isCounterToHight} counter={counter} />
-      ) : (
-        <SettingsMenu addEndValue={() => {}} addStartValue={() => {}} />
-      )}
-      <div className={s.buttons}>
-        <Button
-          callBack={increaseCounter}
-          disabled={isCounterToHight}
-          title="inc"
-        />
-        <Button callBack={onClickOpenSettingsHeandler} title="set" />
-        <Button
-          callBack={() => {
-            setCounter(0);
-          }}
-          disabled={!isCounterToHight}
-          title="reset"
-        />
-      </div>
+    <div>
+      <h1>Counter</h1>
+      <button>Increment</button>
+      <button>Reset</button>
     </div>
   );
 }
