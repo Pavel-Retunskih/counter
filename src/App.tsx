89b62443944c477e3c568ded2a6d17d18942fdbb
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2024-05-03 00:49:15 +0300

    pure logik is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
 import { Counter } from "./Counter";
-
+export type DataType = {
+  counter: number;
+  startValue: number;
+  endValue: number;
+};
 function App() {
+  const data: DataType = {
+    counter: 0,
+    startValue: 0,
+    endValue: 10,
+  };
+  //********************************************************************** */
+  const [counterValues, setCounterValues] = useState(data);
+
+  //************************************************************************* */
+  const setStartValue = (value: number) => {
+    setCounterValues({ ...counterValues, startValue: value, counter: value });
+  };
+  const setEndValue = (value: number) => {
+    setCounterValues({ ...counterValues, endValue: value });
+  };
+  const incrementCounter = () => {
+    setCounterValues({ ...counterValues, counter: counterValues.counter + 1 });
+  };
+  const resetCounter = () => {
+    setCounterValues({ ...counterValues, counter: counterValues.startValue });
+  };
+  //*************************************************************************** */
+
   return (
     <div className="container">
-      <Counter />
+      <Counter
+        counter={counterValues.counter}
+        endValue={counterValues.endValue}
+        startValue={counterValues.startValue}
+        setStartValue={setStartValue}
+        setEndValue={setEndValue}
+        incrementCounter={incrementCounter}
+        resetCounter={resetCounter}
+      />
     </div>
   );
 }
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,9 +1,54 @@
-export function Counter() {
+import { ChangeEvent, useState } from "react";
+
+type CounterPropsType = {
+  counter: number;
+  endValue: number;
+  startValue: number;
+  setStartValue: (value: number) => void;
+  setEndValue: (value: number) => void;
+  incrementCounter: () => void;
+  resetCounter: () => void;
+};
+
+export function Counter({
+  counter,
+  endValue,
+  startValue,
+  setStartValue,
+  setEndValue,
+  incrementCounter,
+  resetCounter,
+}: CounterPropsType) {
+  const [maxValue, setMaxValue] = useState(endValue);
+  const [minValue, setMinValue] = useState(startValue);
+  const valueReached = counter >= endValue;
+  console.log(valueReached);
+
+  const onClickSetHeandler = () => {
+    setEndValue(maxValue);
+    setStartValue(minValue);
+  };
+
   return (
     <div>
-      <h1>Counter</h1>
-      <button>Increment</button>
-      <button>Reset</button>
+      {!valueReached && <h1>{counter}</h1>}
+      <button onClick={incrementCounter}>Increment</button>
+      <button onClick={resetCounter}>Reset</button>
+      <input
+        type="number"
+        value={maxValue}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          setMaxValue(+e.currentTarget.value);
+        }}
+      />
+      <input
+        type="number"
+        value={minValue}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          setMinValue(+e.currentTarget.value);
+        }}
+      />
+      <button onClick={onClickSetHeandler}>Set</button>
     </div>
   );
 }
